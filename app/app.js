3d377b712b63cbcc842eb867ec9f44d61bb4c363
Lucas Santos <lhs.santoss@gmail.com>
2017-05-10 23:03:48 -0300

    :sparkles: Add base files
    Signed-off-by: Lucas Santos <lhs.santoss@gmail.com>

---
+++ app/app.js
@@ -0,0 +1,5 @@
+const React = require('react')
+const ReactDOM = require('react-dom')
+const ReactMap = require('./components/reactMap')
+
+ReactDOM.render(<ReactMap />, document.getElementById("app"))
---
+++ app/components/reactMap.js
@@ -0,0 +1,11 @@
+const React = require('react')
+
+let reactMap = React.createClass({
+  render: () => {
+    return (
+      <h1>hello</h1>
+    )
+  }
+})
+
+module.exports = reactMap
