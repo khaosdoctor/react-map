b96753704457ede4da36c8c32d80e3bc25f9a211
Lucas Santos <lhs.santoss@gmail.com>
2017-05-11 22:41:31 -0300

    :fire: Removing everything to publish

--- app/app.js
+++
@@ -1,5 +0,0 @@
-const React = require('react')
-const ReactDOM = require('react-dom')
-const ReactMap = require('./components/reactMap')
-
-ReactDOM.render(<ReactMap />, document.getElementById('app'))
